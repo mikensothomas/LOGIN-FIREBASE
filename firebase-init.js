1588e9070d93fb1e21f56f5424d4b72ef32b2b89
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-01-04 05:25:53 -0300

    Commit

--- firebase-init.js
+++ firebase-init.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBu1JxiEpP6kML3hh2ulPY7RldPwwnc87w",
-    authDomain: "projetotcc-4d7af.firebaseapp.com",
-    projectId: "projetotcc-4d7af",
-    storageBucket: "projetotcc-4d7af.appspot.com",
-    messagingSenderId: "263705406504",
-    appId: "1:263705406504:web:b3ab7d4ef31b1342a1191d"
-};
+    apiKey: "AIzaSyDkAw90H9tuwb6WoVYhp3ikbYDKgNumqUE",
+    authDomain: "projeto-tcc-a6985.firebaseapp.com",
+    projectId: "projeto-tcc-a6985",
+    storageBucket: "projeto-tcc-a6985.appspot.com",
+    messagingSenderId: "257238440256",
+    appId: "1:257238440256:web:6b2320478a9c794df7c2a5"
+  };
 firebase.initializeApp(firebaseConfig);
